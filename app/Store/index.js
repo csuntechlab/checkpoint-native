4caa4955f4c9fb59eb4ec49e0e7871e8071be2f9
chippingcolt <chippingcolt1a@gmail.com>
2019-01-25 13:22:47 -0800

    quick fix

--- app/Store/index.js
+++ app/Store/index.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 //module imports
-import example from './modules/example'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        example
     },
     // strict: debug,
     // plugins: debug ? [createLogger()] : []
